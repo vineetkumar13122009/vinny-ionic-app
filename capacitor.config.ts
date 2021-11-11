f90e557f9188994d21df55bd2ad79e82ec35dc73
Vineet Kumar <vineetkumar13122009@gmail.com>
2021-11-11 10:24:21 +0530

    InitialStart

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vinny-app1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
